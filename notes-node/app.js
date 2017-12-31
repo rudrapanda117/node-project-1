e2dbef7eab853bbe42afa12f6db727ce49cc9343
Rudrasish <rudrapanda117@gmail.com>
2017-12-31 18:40:35 +0530

    accepting user input

--- notes-node/app.js
+++ notes-node/app.js
@@ -5,3 +5,26 @@ const fs = require('fs');
 const _ = require('lodash');
 
 const notes = require('./notes.js');
+
+console.log(process.argv);
+
+
+let command = process.argv[2];
+console.log('Command: ', command);
+
+if (command === 'add') {
+    console.log('adding new note');
+
+} else if (command === 'list') {
+    console.log('listing all notes');
+
+} else if (command === 'read') {
+    console.log(' reading notes');
+
+} else if (command === 'remove') {
+    console.log('remove notes');
+
+} else {
+    console.log('Command not recognized');
+
+}
