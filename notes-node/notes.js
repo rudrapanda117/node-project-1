d4a300c6ab93e68d51636f3819d743735dbe0a68
Rudrasish <rudrapanda117@gmail.com>
2017-12-31 20:13:10 +0530

    Addding and saving notes into and out of files

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,7 +1,34 @@
 console.log('Starting notes.js');
 
+const fs = require('fs');
+
 var addNote = (title, body) => {
     console.log('Adding note ', title, body);
+    let notes = [];
+    try {
+        let notesString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(notesString);
+    } catch (e) {}
+
+    let note = {
+        title,
+        body
+    };
+
+
+    var duplicateNotes = notes.filter((note) => note.title === title);
+
+    if (duplicateNotes.length === 0) {
+        notes.push(note);
+        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    } else  {
+        console.log('Avoiding adding same title: ', title);
+        
+    }
+
+
+
+
 };
 
 var getAll = () => {
