1e77220b3fdf33367fda9393b0a05118b3788faf
Rudrasish <rudrapanda117@gmail.com>
2017-12-31 14:52:59 +0530

    OS Module - Getting logged in username in computer

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,10 +2,16 @@ console.log('Starting App.');
 
 
 const fs = require('fs');
+const os = require('os');
 
-fs.appendFile('greetings.txt', 'Hello World!', function(err) {
-    if(err) {
+let user = os.userInfo();
+
+console.log(user);
+
+
+fs.appendFile('greetings.txt', `Hello ${user.username} !`, function (err) {
+    if (err) {
         console.log('Unable to write file');
-        
+
     }
 });
