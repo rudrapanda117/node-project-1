e76b75054adb01e243c663d9a3a1b44d04c4973e
Rudrasish <rudrapanda117@gmail.com>
2018-01-01 19:30:28 +0530

    Fetching weather

--- weather-app/app.js
+++ weather-app/app.js
@@ -18,10 +18,28 @@ const argv = yargs
 
 
 geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-    if(errorMessage) {
-        console.log(errorMessage);        
+    if (errorMessage) {
+        console.log(errorMessage);
     } else {
-        console.log(JSON.stringify(results, undefined, 2));        
+        console.log(JSON.stringify(results, undefined, 2));
     }
-})
+});
 
+
+//a2b0348f7fa5d9fb164cc5c3ff9a4f8a
+
+const request = require('request');
+
+request({
+    url: 'https://api.darksky.net/forecast/a2b0348f7fa5d9fb164cc5c3ff9a4f8a/12.9496676,77.7012903?exclude=minutely,hourly,daily,alerts,flags',
+    json: true
+}, (error, response, body) => {
+    if (error) {
+        console.log('Unable to connect to forecast.io server.');
+    } else if (response.statusCode === 400) {
+        console.log('Unable to fetch WebAuthnAssertion.');
+    } else if (response.statusCode === 200) {
+        console.log(body.currently.temperature);
+    }
+
+});
