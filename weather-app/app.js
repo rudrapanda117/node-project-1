e5667137211e3232aeeca971ef3982d59809f17b
Rudrasish <rudrapanda117@gmail.com>
2018-01-01 18:19:15 +0530

    Error handling

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,34 +2,37 @@ const request = require('request');
 const yargs = require('yargs');
 
 const argv = yargs
-.options({
-    a: {
-        demand: true,
-        alias: 'address',
-        describe: 'Address to fetch weather for',
-        string: true
-    }
-})
-.help()
-.alias('help' ,'h')
-.argv;
-
-
-console.log(argv);
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
 
 let address = argv.address;
 let encodedAddress = encodeURIComponent(address);
-
-console.log(encodedAddress);
-
 
 request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
 }, (error, response, body) => {
-  //  console.log(JSON.stringify(response, undefined, 2));
-  console.log(`Address: ${body.results[0].formatted_address}`);
-  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-  console.log(`Latitude: ${body.results[0].geometry.location.lng}`);
-  
+    if (error) {
+        console.log('Unable to connect to google server!');
+    } else if (body.status === 'ZERO_RESULTS') {
+        console.log('Unable to find the address!');
+    }
+    else if (body.status === 'INVALID_REQUEST') {
+        console.log(body.error_message);
+    } else if (body.status === 'OK') {
+        //  console.log(JSON.stringify(response, undefined, 2));
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+        console.log(`Latitude: ${body.results[0].geometry.location.lng}`);
+    }
+
 })
