84e2cab82b7b2580c1fc9d758b59586093ce9a10
Rudrasish <rudrapanda117@gmail.com>
2018-01-01 15:36:05 +0530

    accessing lat and long from response body

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,6 +4,9 @@ request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=munnekolala,%20marathalli,bengaluru',
     json: true
 }, (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 2));
-    
+  //  console.log(JSON.stringify(response, undefined, 2));
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lng}`);
+  
 })
