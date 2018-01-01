c2850ab59b984b44860725dd06eb91b99d692180
Rudrasish <rudrapanda117@gmail.com>
2018-01-01 20:33:04 +0530

    Composing different promises

--- weather-app/playground/promise.js
+++ weather-app/playground/promise.js
@@ -1,12 +1,27 @@
-let somePromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('Hey . it worked');
-       // reject(' Unable to fulfill promise.')
-    }, 2000);
-});
+let asyncAdd = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (typeof a === 'number' && typeof b === 'number') {
+                resolve(a + b);
+            } else {
+                reject('Arguments must be number');
+            }
+        }, 2000);
+    });
+}
 
-somePromise.then((result) => {
-    console.log('result', result);
-}, (errorMessage) => {
-    console.log('error:', errorMessage);
-});
+let promise = asyncAdd(1, 2);
+
+promise.then((result) => {
+        console.log(result);
+        return asyncAdd(result , 4);
+    },
+    (errorMessage) => {
+        console.log(errorMessage);
+    })
+    .then((result) => {
+        console.log(result);
+    },
+    (errorMessage) => {
+        console.log(errorMessage);
+    })
