c81c3007775d3030e2d330c5b477ee1c4e86a583
Rudrasish <rudrapanda117@gmail.com>
2018-01-02 00:08:09 +0530

    Custom middle ware

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 let app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
-
-app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
@@ -16,7 +15,22 @@ hbs.registerHelper('screamIt', (text) => {
     return text.toUpperCase();
 });
 
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    console.log(`${now}: ${req.method} ${req.url}`);  
+    let log =   `${now}: ${req.method} ${req.url}`;
+    fs.appendFile('Server.log',log+'\n', (err) => {
+        console.log('Unable to append to server.log .');        
+    })
+    next();
+});
 
+app.use((req, res, next) => {
+    //res.render('maintenance.hbs');
+    next();
+});
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (request, response) => {
     response.render('home.hbs', {
