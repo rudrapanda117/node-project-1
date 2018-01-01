6b6efc82cf5deb9d002f31362a22f8b509f0e078
Rudrasish <rudrapanda117@gmail.com>
2018-01-01 23:49:46 +0530

    Advance usage of handlebar templating

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -3,32 +3,42 @@ const hbs = require('hbs');
 
 let app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+
+
 app.get('/', (request, response) => {
-    response.render('home.hbs',{
+    response.render('home.hbs', {
         pageTitle: 'Home Page',
-        currentYear: new Date().getFullYear(),
         welcomeMessage: 'Welcome to my website'
     });
 });
 
 app.get('/about', (request, response) => {
-    response.render('about.hbs',{
-        pageTitle: 'About Page',
-        currentYear: new Date().getFullYear()
+    response.render('about.hbs', {
+        aboutMessage: 'This is about page',
+        pageTitle: 'About Page'
     });
 });
 
 app.get('/bad', (request, response) => {
     response.send({
-            errorMessage: 'Unable to handle request'
+        errorMessage: 'Unable to handle request'
     });
 });
 
 app.listen(8900, () => {
     console.log('Server is up on port 3000');
-    
+
 });
