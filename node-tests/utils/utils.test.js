c6f62322a52aa20312e958fe4945b82d9d4bbc58
Rudrasish <rudrapanda117@gmail.com>
2018-01-06 21:37:12 +0530

    Assertion

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,3 +1,5 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 
@@ -6,16 +8,15 @@ describe('util test', () => {
 
     it('should add two numbers', () => {
         var res = utils.add(33, 11);
-        if (res != 44) {
-            throw new Error(` Expected 44, but got ${res}`)
-        }
+       expect(res).toBe(44);
+    //  /  expect(res).toNotBeA('number');
+
     });
 
     it('should square a numbers', () => {
         var res = utils.square(3);
-        if (res != 9) {
-            throw new Error(` Expected 9, but got ${res}`)
-        }
+        expect(res).toBe(9);
+       // expect(res).toBeA('number')
     });
 
 });
